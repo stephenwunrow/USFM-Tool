604cbd89bf47622514ccc6e395da92dcb386e407
stephenwunrow <stephen.wunrow@my.wheaton.edu>
2026-05-21 14:07:43 -0500

    fixing count formatting

--- search_texts.py
+++ search_texts.py
@@ -107,7 +107,7 @@ def search_verses(query, version="ULT"):
     results = (
         f"<div class='result-count'>"
         f"<span class='ref'>Found {match_count} matches.</span>"
-        f"</div></div>"
+        f"</div>"
         + results
     )
 
