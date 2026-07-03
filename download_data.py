58733cae3e952570f8a4b917ade645a12ade30c2
stephenwunrow <stephen.wunrow@my.wheaton.edu>
2026-07-03 11:05:24 -0500

    adding path for downloads

--- download_data.py
+++ download_data.py
@@ -1,9 +1,11 @@
 from note_lookup import download_notes_files
 from search import download_text_files
 from texts import main
+import os
 
 
 def download_data():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     download_notes_files()
     print('Downloaded notes files')
     download_text_files()
